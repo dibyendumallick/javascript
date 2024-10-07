c97ff2913268120430d3505c53c4d5609663306c
dibyendumallick <147793412+dibyendumallick@users.noreply.github.com>
2024-10-07 14:04:22 +0000

    7oct7pm

---
+++ basics/numbers.js
@@ -0,0 +1,15 @@
+//---------------maths-------------------
+const num=-45.2;
+console.log(Math.abs(num));
+console.log(Math.ceil(num));
+console.log(Math.floor(num));
+/*console.log(Math.abs.sqrt(num)); //it wont work because Math.abs.sqrt is not a function*/
+console.log(Math.min(4,6,97,0));
+console.log(Math.floor(Math.random()*10+1));
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min);//between min and max value
+
+
